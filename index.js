9dae4e9cfbab841b8080ad3d9d8ed513241fc45c
Pawan Kumar <ratwanapawan@gmail.com>
2023-05-20 11:41:08 +0530

    setup ejs engine and create a view for home

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express =   require('express');
 const app     =   express()
 const port    =   8000;
-
+//  use express router
 app.use('/',require('./routes'))
 
+//setup the view engine
+app.set('view engine','ejs');
+app.set('views','./views')
 
 app.listen(port, (err)=>{
         if(err){
